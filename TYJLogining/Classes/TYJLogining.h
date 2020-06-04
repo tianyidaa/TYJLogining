1c2f5303b1d3e6c9d6629d75becc27ab88d5d068
田益杰 <tianyijie@tianyijiedeMacBook-Pro.local>
2020-06-04 12:23:27 +0800

    init TYJLogining

---
+++ TYJLogining/Classes/TYJLogining.h
@@ -0,0 +1,17 @@
+//
+//  TYJLogining.h
+//  TYJLogining_Example
+//
+//  Created by 田益杰 on 2020/6/4.
+//  Copyright © 2020 tian_yida. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TYJLogining : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TYJLogining/Classes/TYJLogining.m
@@ -0,0 +1,13 @@
+//
+//  TYJLogining.m
+//  TYJLogining_Example
+//
+//  Created by 田益杰 on 2020/6/4.
+//  Copyright © 2020 tian_yida. All rights reserved.
+//
+
+#import "TYJLogining.h"
+
+@implementation TYJLogining
+
+@end
